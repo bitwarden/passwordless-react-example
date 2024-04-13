5a0c258a88ebff5d77f8a635d7257b9e2538e578
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-04-13 09:45:11 +0200

    Fix alias parameter not working properly.

--- src/configuration/PasswordlessOptions.js
+++ src/configuration/PasswordlessOptions.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL = 'http://localhost:5013';
-export const PASSWORDLESS_API_KEY = 'jonashendrickx1:public:ed1efdf1c5f4434696ebbca72de12958';
-export const PASSWORDLESS_API_URL = 'http://localhost:7001';
+export const BACKEND_URL = 'https://pwdemo.lesspassword.dev';
+export const PASSWORDLESS_API_KEY = 'jonasapp1:public:a13666586ea448958c38cdec76eaaace';
+export const PASSWORDLESS_API_URL = 'https://v4.passwordless.dev';
