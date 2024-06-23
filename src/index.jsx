9b9cbdeae53df84049a86381ceb8c8281c399fc4
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-06-23 09:47:10 +0200

    Remove web-vitals dependency. It is not being used in a useful way.

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import {AuthProvider} from "./context/AuthProvider";
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root')
@@ -18,5 +17,3 @@ root.render(
       </AuthProvider>
   </React.StrictMode>
 );
-
-reportWebVitals();
